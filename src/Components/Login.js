5409d54da0439f11ada2325695227e146c4424f2
Neha Joshi <nehajoshigkp@gmail.com>
2021-10-07 20:48:31 +0530

    logo and button added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,13 @@ import styled from 'styled-components';
 function Login(props) {
     return (
         <Container>
+            <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="logo" />
+                    <SignUp>GET ALL THERE</SignUp>
+                    <Description>Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and the Disney Bundle will increase by $1.</Description>
+                </CTA>
+            </Content>
             <BgImage />
         </Container>
     )
@@ -42,7 +49,46 @@ const BgImage = styled.div`
     left: 0;
     z-index: -1;
 `
+const CTA = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
 
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display: block;
+    width: 100%;
+
+`
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover {
+        background-color: #0483ee;
+        cursor: pointer;
+    }
+`
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-weight: 1.5px;
+    
+
+`
 
 export default Login
 
